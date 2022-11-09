28dc29f6f65c45c0259282f58a563badd03ef443
eduardnegruta1 <eduard.negruta@ramsoft.com>
2022-11-09 19:04:41 +0100

    rebuild

--- src/shared/decoders/decodeJPEGLossless.js
+++ src/shared/decoders/decodeJPEGLossless.js
@@ -18,44 +18,8 @@ export function initialize(decodeConfig) {
   });
 }
 
-async function decodeJPEGLossless(imageFrame, pixelData) {
-  await initialize();
-
-  // check to make sure codec is loaded
-  if (
-    typeof local.jpeg === 'undefined' ||
-    typeof local.jpeg.lossless === 'undefined' ||
-    typeof local.jpeg.lossless.Decoder === 'undefined'
-  ) {
-    throw new Error('No JPEG Lossless decoder loaded');
-  }
-
-  const byteOutput = imageFrame.bitsAllocated <= 8 ? 1 : 2;
-  // console.time('jpeglossless');
-  const buffer = pixelData.buffer;
-  const decoder = new local.jpeg.lossless.Decoder();
-  const decompressedData = decoder.decode(
-    buffer,
-    pixelData.byteOffset,
-    pixelData.length,
-    byteOutput
-  );
-  // console.timeEnd('jpeglossless');
-
-  if (imageFrame.pixelRepresentation === 0) {
-    if (imageFrame.bitsAllocated === 16) {
-      imageFrame.pixelData = new Uint16Array(decompressedData.buffer);
-
-      return imageFrame;
-    }
-    // untested!
-    imageFrame.pixelData = new Uint8Array(decompressedData.buffer);
-
-    return imageFrame;
-  }
-  imageFrame.pixelData = new Int16Array(decompressedData.buffer);
-
-  return imageFrame;
+async function decodeJPEGLossless() {
+  return console.log('STOP');
 }
 
 export default decodeJPEGLossless;
